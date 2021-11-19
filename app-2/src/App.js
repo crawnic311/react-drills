dcb4aa5b53113ac7d82b0cf128af091f438c673f
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-19 11:44:19 -0700

    Drill 2 completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 //Class Component
-class App extends React {
+class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      dataArray: []
+      dataArray: ["Axe", "candy", "Corn", "Water Bison", "Philly Cheese Steak"]
     }
+  }
+
+  render() {
+    let foodsToDisplay = this.state.dataArray
+    return (
+      <div>
+        <h2>{foodsToDisplay[0]}</h2>
+        <h2>{foodsToDisplay[1]}</h2>
+        <h2>{foodsToDisplay[2]}</h2>
+        <h2>{foodsToDisplay[3]}</h2>
+        <h2>{foodsToDisplay[4]}</h2>
+      </div>
+    )
   }
 }
 
