69dac92c71cea857e3edc17c6a17a64d82e5c95f
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-19 11:25:26 -0700

    Drill 1 Class Component Built

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//class componenet
+class App extends Component {
+  constructor() {
+    super()
 
+    this.state = {
+      userInput: undefined
+    }
+  }
+
+  changeHandler(e) {
+    this.setState({userInput: e})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={(e) => this.changeHandler(e.target.value)}></input>
+        <p>{JSON.stringify(this.state.userInput)}</p>
+      </div>
+    ) 
+  }
+}
+ 
 export default App;
+
